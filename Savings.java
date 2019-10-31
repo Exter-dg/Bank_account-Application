979cc88fdc25761855ea2c4265829bce69f24e71
Exter-dg <prkandmap23@rocketmail.com>
2019-10-31 14:30:38 +0530

    Basic Account Class Created

--- Savings.java
+++ Savings.java
@@ -4,4 +4,10 @@ public class Savings extends Account
 {
   int safetyDepositBoxID;
   int safetyDepositBoxKey;
+
+  public Savings(String name, String sSN , double initDeposit)
+  {
+    super(name , sSN , initDeposit);
+    accountNumber = "1" + accountNumber;
+  }
 }
