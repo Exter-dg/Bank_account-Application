2e43a70088139d2ccd33e94d0ca1c0163332c8bb
khushi2912 <foodaholicclasher@gmail.com>
2019-11-01 17:25:27 +0530

    Checking Started

--- Checking.java
+++ Checking.java
@@ -2,11 +2,33 @@ package bankaccountapp;
 
 public class Checking extends Account
 {
-  int debitCardNumber;
-  int debitCardPIN;
-  
-  Checking()
+  private int debitCardNumber;
+  private int debitCardPIN;
+ 
+  Checking(String name, String sSn, double initDeposit)
   {
-
+    System.out.println("ACCOUNT TYPE: CHECKING");
+    super(name,ssn,balance);
+    accountNumber="2"+accountNumber;
+    setDebitCard();
   }
+
+  public void setRate()
+  {
+    rate=0.15*getBaseRate();
+  }
+	
+  public void showInfo()
+  {
+    System.out.println("ACCOUNT TYPE:CHECKING");
+    super.showInfo();
+    System.out.println("DEBIT CARD NUMBER:"+debitCardNumber+"\nDEBIT CARD PIN:"+debitCardPin); 
+  }
+	
+  private void setDebitCard()
+  {
+    debitCardNumber=(int)(Math.random()*Math.pow(10,12));
+    debitCardPin=(int)(Math.random()*Math.pow(10,4));
+  }
+
 }
